0ba1fe4c105ddc0f582d3486cbef818a8d24df4f
Gamadv <lealdev.py@gmail.com>
2024-07-06 13:49:20 -0300

    📝 docs(Swagger): Add missing decorators

--- backend/src/controllers/fetch-statistic.controller.ts
+++ backend/src/controllers/fetch-statistic.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { StatisticsService } from 'src/services/statistic.service';
 
+@ApiTags('Statistics')
 @Controller('statistics')
 export class StatisticsController {
   constructor(private readonly statisticsService: StatisticsService) {}
 
   @Get('equipment')
+  @ApiOperation({ summary: 'Get equipment Statictics by ID' })
+  @ApiResponse({ status: 200, description: 'Return all equipment statictics.' })
   async getStatisticsByEquipmentId(@Query('equipmentId') equipmentId: string): Promise<any[]> {
     return await this.statisticsService.getStatisticsByEquipmentId(equipmentId);
   }
